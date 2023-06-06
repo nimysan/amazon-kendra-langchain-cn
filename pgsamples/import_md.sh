cbbbac5440c346c643fb744322af58b760094a99
yexw <yexw@amazon.com>
2023-06-06 15:25:23 +0800

    增加导入操作

---
+++ pgsamples/import_md.sh
@@ -0,0 +1,9 @@
+export FAQ_PATH=xx
+export COLLECTION_NAME=yyy
+
+export OPENAI_API_KEY=xxx
+export PGVECTOR_HOST=postgres.cypjqpec31mg.ap-southeast-1.rds.amazonaws.com
+export PGVECTOR_PORT=5432
+export PGVECTOR_PASSWORD=4loc8YDMY5NIlzpv2X9j
+
+python3 import_md_vg.py
