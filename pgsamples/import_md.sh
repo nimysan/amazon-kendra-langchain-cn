c9603f28438c0761835919bb114e8567f6f27706
yexw <yexw@amazon.com>
2023-06-06 16:35:31 +0800

    hide

--- pgsamples/import_md.sh
+++ pgsamples/import_md.sh
@@ -4,6 +4,6 @@ export COLLECTION_NAME=yyy
 export OPENAI_API_KEY=xxx
 export PGVECTOR_HOST=postgres.cypjqpec31mg.ap-southeast-1.rds.amazonaws.com
 export PGVECTOR_PORT=5432
-export PGVECTOR_PASSWORD=4loc8YDMY5NIlzpv2X9j
+export PGVECTOR_PASSWORD=xxxx
 
 python3 import_md_vg.py
